9fb3f3bbee037dca23245661f3c644f8fd2b03f5
anikmondol <anikmondol558363@gmil.com>
2024-05-28 09:21:05 +0600

    ByteBlaze projects full done

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ import rehypeRaw from "rehype-raw";
 
 const Content = () => {
     const blog = useLoaderData();
-    const { cover_image, title, tags, body_html} = blog;
+    const { cover_image, title, tags, body_html, url} = blog;
     return (
         <div className=" mx-auto transition border-2 p-2  border-opacity-30   group hover:no-underline focus:no-underline px-16">
             <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || placeholderImage} />
@@ -15,7 +15,7 @@ const Content = () => {
                     
                 </div>
             <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                <a href={url} target="_blank" className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</a>
                 <Markdown rehypePlugins={[rehypeRaw]}>{body_html}</Markdown>
                
             </div>
--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import { deleteBlog, getBlogs } from "../utils";
+import Blog from "./Blog";
+import EmptyState from "../components/EmptyState";
 
 
 const Bookmarks = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() =>{
+        const storedBlogs = getBlogs();
+        setBlogs(storedBlogs)
+    }, [])
+
+    const handleDelete = id =>{
+        deleteBlog(id);
+        const storedBlogs = getBlogs();
+        setBlogs(storedBlogs);
+    }
+
+    if(blogs.length < 1) return <EmptyState address={'/blogs'} label={'Go to Blogs'} message='No Bookmarks Available'></EmptyState>
+
+
     return (
-        <div>
-            <h1>Bookmarks Page</h1>
-        </div>
+        <div className="grid justify-center my-8 grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+                
+                {
+                    blogs.map((blog, index) => <Blog handleDelete = {handleDelete}
+                    deletable={true} key={index} blog={blog}></Blog>)
+                }
+                
+                
+            </div>
     );
 };
 
